5d9606bbc93af1e4d877721c1c8d2472e684a2de
nguyencanh2000 <1851161689@e.tlu.edu.vn>
2021-03-31 20:32:20 +0700

    up FoodDetail

---
+++ BTL_OrderFood/app/src/main/java/vn/example/btl_orderfood/FoodDetail.java
@@ -0,0 +1,104 @@
+package vn.example.btl_orderfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+
+import vn.example.btl_orderfood.ui.Food;
+
+public class FoodDetail extends AppCompatActivity {
+
+    TextView food_name,food_price,food_description;
+    ImageView food_image;
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    FloatingActionButton btncart;
+    ElegantNumberButton numberButton;
+
+    String foodId="";
+
+    FirebaseDatabase database;
+    DatabaseReference foods;
+    Food currentFood;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_detail);
+
+        //Firebase
+
+        database = FirebaseDatabase.getInstance();
+        foods = database.getReference("Foods");
+
+        //init view
+        numberButton = (ElegantNumberButton)findViewById(R.id.number_button);
+        btncart = (FloatingActionButton)findViewById(R.id.btncart);
+
+        /*btncart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Database(getBaseContext()).addToCart(new Order(foodId,
+                        currentFood.getName(),
+                        numberButton.getNumber(),
+                        currentFood.getPrice(),
+                        currentFood.getDiscount()));
+                Toast.makeText(FoodDetail.this, "Added to cart", Toast.LENGTH_SHORT).show();
+            }
+        });*/
+
+        food_description = (TextView)findViewById(R.id.food_description);
+        food_name = (TextView)findViewById(R.id.food_name);
+        food_price = (TextView)findViewById(R.id.food_price);
+
+        food_image = (ImageView)findViewById(R.id.img_food);
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.collapsing);
+        collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.ExpandedAppbar);
+        collapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.CollapsedAppbar);
+
+        //get foodId
+
+        if(getIntent() != null)
+            foodId = getIntent().getStringExtra("FoodId");
+        if(!foodId.isEmpty())
+        {
+            getDetailFood(foodId);
+        }
+    }
+
+    private void getDetailFood(String foodId) {
+        foods.child(foodId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Food food = dataSnapshot.getValue(Food.class);
+                //set Image
+                Picasso.with(getBaseContext()).load(food.getImage()).into(food_image);
+
+                collapsingToolbarLayout.setTitle(food.getName());
+                food_price.setText(food.getPrice());
+                food_name.setText(food.getName());
+                food_description.setText(food.getDescription());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
--- BTL_OrderFood/app/src/main/java/vn/example/btl_orderfood/FoodList.java
+++ BTL_OrderFood/app/src/main/java/vn/example/btl_orderfood/FoodList.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -63,6 +64,10 @@ public class FoodList extends AppCompatActivity {
                     @Override
                     public void onClick(View view, int position, boolean isLongClick) {
                         Toast.makeText(FoodList.this,""+local.getName(),Toast.LENGTH_SHORT).show();
+                        //start activity
+                        Intent foodDetails = new Intent(FoodList.this,FoodDetail.class);
+                        foodDetails.putExtra("FoodId",adapter.getRef(position).getKey());
+                        startActivity(foodDetails);
                     }
                 });
             }
